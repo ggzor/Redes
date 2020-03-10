62372072b8220b56d9281e5b2fa5ad3ae16ea45a
ggzor <30713864+ggzor@users.noreply.github.com>
2020-03-09 20:56:53 -0600

    Adding args name retrieving

--- lib/argsparse.tcl
+++ lib/argsparse.tcl
@@ -1,6 +1,40 @@
 package provide argsparse 0.1
 
 namespace eval ::Args {
+    # Returns the name of the arguments in the configuration
+    proc vars config {
+        Args::args $config vars _ _
+        return $vars
+    }
+
+    # Returns all the args in config, separated
+    #   positionalVar will contain the postional variables in order
+    #   optionsVar will contain the options with its defaults
+    proc args {config allVar positionalVar optionsVar} {
+        uplevel "set $allVar {}"
+        uplevel "set $positionalVar {}"
+        uplevel "set $optionsVar {}"
+        
+        while {[llength $config] > 0} {
+            set first  [lindex $config 0]
+            set value  [lindex $config 1]
+
+            if [regexp {^\-(\S+)} $first _ option] {
+                # Search for options
+                uplevel lappend $allVar      $option
+                uplevel lappend $optionsVar "$option $value"
+
+                set config [lrange $config 2 end]
+            } else {
+                # Search for positional arguments
+                uplevel lappend $allVar        $first
+                uplevel lappend $positionalVar $first
+                
+                set config [lrange $config 1 end]
+            }
+        }
+    }
+
     # Given an args configuration and a parameter list sets the variables
     # to their parameter values or defaults in the current scope.
     proc parse { parameters config } {
